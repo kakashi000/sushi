6f46de22caa50cd0ff7545905c997831e499de71
kakashi <jky200395@gmail.com>
2019-04-18 22:13:28 +0800

    add =anime command

---
+++ discord_commands/anime.js
@@ -0,0 +1,79 @@
+/* eslint-disable prefer-destructuring */
+const got = require('got');
+const bot = require('../bot.js');
+const config = require('../config/config.json');
+
+const command = {};
+
+command.name = 'anime';
+
+command.action = async (msg, args) => {
+  const options = {
+    baseUrl: 'https://kitsu.io/api/edge/',
+    headers: {
+      Accept: 'application/vnd.api+json',
+      'Content-Type': 'application/vnd.api+json',
+    },
+    json: true,
+    query: {
+      'filter[text]': args.join(''),
+    },
+  };
+
+  const response = await got('anime', options);
+
+  if (!response.body.data[0]) {
+    return msg.channel.createMessage('Anime not found~');
+  }
+
+  const anime = {};
+  anime.title = response.body.data[0].attributes.canonicalTitle;
+  // cut off synopsis if it's too long
+  const synopsis = response.body.data[0].attributes.synopsis;
+  anime.synopsis = (synopsis.length < 700) ? synopsis : `${synopsis.substr(0, 700)}...`;
+  anime.thumbnail = response.body.data[0].attributes.posterImage.small;
+  anime.type = response.body.data[0].attributes.subtype;
+
+  const animeInfo = {
+    embed: {
+      title: 'Kitsu Anime Search',
+      color: config.color,
+      author: {
+        icon_url: bot.user.avatarURL,
+      },
+      fields: [
+        {
+          name: 'Title',
+          value: anime.title,
+        },
+        {
+          name: 'Type',
+          value: anime.type,
+        },
+        {
+          name: 'Synopsis',
+          value: anime.synopsis,
+        },
+      ],
+      thumbnail: {
+        url: anime.thumbnail,
+      },
+      timestamp: new Date(),
+      footer: {
+        icon_url: bot.user.avatarURL,
+        text: 'powered by Kitsu.io',
+      },
+    },
+  };
+
+  return msg.channel.createMessage(animeInfo);
+};
+
+command.options = {
+  aliases: ['a'],
+  cooldown: 3000,
+  description: 'Search for an anime on Kitsu.io!',
+  usage: 'anime yuru yuri',
+};
+
+module.exports = command;
