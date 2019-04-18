1f69dc6bcc315fe19246858011e2dd8d37c52c4e
kakashi <jky200395@gmail.com>
2019-04-18 19:47:17 +0800

    set defaultImageSize to 512, restMode to false

--- bot.js
+++ bot.js
@@ -1,13 +1,18 @@
 const Eris = require('eris');
 const config = require('./config/config.json');
 
-const botOptions = { restMode: true };
+const botOptions = {
+  defaultImageSize: 512,
+  restMode: false,
+};
+
 const commandOptions = {
   prefix: ['='],
   owner: 'kakashi',
   description: 'a bot made using Eris',
   defaultHelpCommand: false,
 };
+
 const bot = new Eris.CommandClient(config.botToken, botOptions, commandOptions);
 
 module.exports = bot;
