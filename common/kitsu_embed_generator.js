4d9e69c78fb4897bbf59e3532276cbd18bde62e6
kakashi <jky200395@gmail.com>
2019-05-14 14:34:07 +0800

    add kitsu character search command

--- common/kitsu_embed_generator.js
+++ common/kitsu_embed_generator.js
@@ -4,49 +4,95 @@ const bot = require('../bot.js');
 function generateEmbed(type, item, msg, currentPage, lastPage) {
   const hasAddReactionsPermission = msg.channel.permissionsOf(bot.user.id).has('addReactions');
 
-  const synopsis = (item.attributes.synopsis.length < 700)
-    ? item.attributes.synopsis : `${item.attributes.synopsis.substr(0, 700)}...`;
+  if (type === 'Anime' || type === 'Manga') {
+    const synopsis = (item.attributes.synopsis.length < 700)
+      ? item.attributes.synopsis
+      : `${item.attributes.synopsis.substr(0, 700)}...`;
 
-  const embed = {
-    embed: {
-      title: `Kitsu ${type} Search`,
-      description: hasAddReactionsPermission
-        ? `Page ${(currentPage)} out of ${(lastPage)}` : '',
-      color: config.color,
-      author: {
-        icon_url: bot.user.avatarURL,
-      },
-      fields: [
-        {
-          name: 'Title',
-          value: (item.attributes.canonicalTitle === '\n\n')
-            ? 'No title found.' : item.attributes.canonicalTitle,
+    const embed = {
+      embed: {
+        title: `Kitsu ${type} Search`,
+        description: hasAddReactionsPermission
+          ? `Page ${(currentPage)} out of ${(lastPage)}` : '',
+        color: config.color,
+        author: {
+          icon_url: bot.user.avatarURL,
         },
-        {
-          name: 'Type',
-          value: (item.attributes.subtype === '\n\n')
-            ? 'No type found.' : item.attributes.subtype,
+        fields: [
+          {
+            name: 'Title',
+            value: (item.attributes.canonicalTitle === '\n\n')
+              ? 'No title found.' : item.attributes.canonicalTitle,
+          },
+          {
+            name: 'Type',
+            value: (item.attributes.subtype === '\n\n')
+              ? 'No type found.' : item.attributes.subtype,
+          },
+          {
+            name: 'Synopsis',
+            value: (synopsis === '\n\n')
+              ? 'No synopsis found.' : synopsis,
+          },
+        ],
+        thumbnail: {
+          url: item.attributes.posterImage.small,
         },
-        {
-          name: 'Synopsis',
-          value: (synopsis === '\n\n')
-            ? 'No synopsis found.' : synopsis,
+        timestamp: new Date(),
+        footer: {
+          icon_url: msg.author.avatarURL,
+          text: hasAddReactionsPermission
+            ? `${msg.author.username} can tap the reaction buttons below to switch pages!`
+            : 'Powered by Kitsu.io',
         },
-      ],
-      thumbnail: {
-        url: item.attributes.posterImage.small,
       },
-      timestamp: new Date(),
-      footer: {
-        icon_url: msg.author.avatarURL,
-        text: hasAddReactionsPermission
-          ? `${msg.author.username} can tap the reaction buttons below to switch pages!`
-          : 'Powered by Kitsu.io',
-      },
-    },
-  };
+    };
 
-  return embed;
+    return embed;
+  }
+
+  if (type === 'Character') {
+    const name = item.canonicalName;
+
+    const description = item.description.length < 700
+      ? item.description
+      : `${item.description.substr(0, 700)}...`;
+
+    const embed = {
+      embed: {
+        title: `Kitsu ${type} Search`,
+        description: hasAddReactionsPermission
+          ? `Page ${(currentPage)} out of ${(lastPage)}` : '',
+        color: config.color,
+        author: {
+          icon_url: bot.user.avatarURL,
+        },
+        fields: [
+          {
+            name: 'Name',
+            value: name,
+          },
+          {
+            name: 'Description',
+            value: description || 'No description found.',
+          },
+        ],
+        thumbnail: {
+          url: item.image
+            ? item.image.original
+            : '',
+        },
+        timestamp: new Date(),
+        footer: {
+          icon_url: msg.author.avatarURL,
+          text: hasAddReactionsPermission
+            ? `${msg.author.username} can tap the reaction buttons below to switch pages!`
+            : 'Powered by Kitsu.io',
+        },
+      },
+    };
+    return embed;
+  }
 }
 
 module.exports = generateEmbed;
---
+++ discord_commands/character.js
@@ -0,0 +1,45 @@
+const Kitsu = require('kitsu');
+const pagination = require('../common/pagination.js');
+const generateKitsuEmbed = require('../common/kitsu_embed_generator.js');
+
+const api = new Kitsu();
+const command = {};
+
+async function generateEmbeds(msg, args) {
+  const response = await api.get('characters', {
+    filter: {
+      name: args.join(' '),
+    },
+  });
+
+  if (!response.data[0]) {
+    return 'Character not found~';
+  }
+
+  const embeds = response.data.map((character, index, data) => (
+    generateKitsuEmbed('Character', character, msg, (index + 1), data.length)
+  ));
+
+  return embeds;
+}
+
+command.action = async (msg, args) => {
+  const characterEmbeds = await generateEmbeds(msg, args);
+  const data = pagination.saveData(
+    msg.id,
+    characterEmbeds,
+    msg.author.id,
+    command.options.reactionButtonTimeout,
+  );
+  return msg.channel.createMessage(data[0]);
+};
+
+command.options = {
+  aliases: ['char'],
+  cooldown: 3000,
+  description: 'Search for an anime or manga character on Kitsu.io!',
+  reactionButtonTimeout: 120000,
+  usage: 'char akaza akari',
+};
+
+module.exports = pagination.addReactionButtons(command, command.options.reactionButtonTimeout);
