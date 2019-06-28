4a253654f4bbecbebbaa3c38762670fbdeec9b0a
kakashi000 <rrk061295@gmail.com>
2019-06-28 15:01:40 +0800

    check if author is bot owner

--- bot/discord_commands/say.js
+++ bot/discord_commands/say.js
@@ -1,7 +1,19 @@
+const config = require('../config/config.json');
+
 const command = {
   name: 'say',
 
-  action: async (msg, args) => msg.channel.createMessage(args.join(' ')),
+  action: (msg, args) => {
+    const authorIsBotOwner = config.botOwnerIDs.includes(msg.author.id);
+
+    if (authorIsBotOwner) {
+      return msg.channel.createMessage(args.join(' '));
+    }
+
+    return msg.channel.createMessage(
+      `:speech_balloon: ${msg.author.username}: ${args.join(' ')}`,
+    );
+  },
 
   options: {
     aliases: ['s', 'echo'],
