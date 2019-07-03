595e3086a330e9c0f5c9fd647b30fb131816da28
kakashi000 <rrk061295@gmail.com>
2019-07-04 01:43:01 +0800

    paginate image search and use googleapis instead of got

--- bot/discord_commands/image.js
+++ bot/discord_commands/image.js
@@ -1,27 +1,20 @@
-const got = require('got');
-const config = require('./../config/config.json');
+const generateLinks = require('../util/generate_image_links.js');
+const { saveData, addReactionButtons } = require('../common/pagination.js');
 
 const command = {
   name: 'image',
 
   action: async (msg, args) => {
-    const author = msg.author.username;
-    const keywords = args.join(' ');
+    const links = await generateLinks(args.join(' '));
 
-    const options = {
-      json: true,
-      query: {
-        cx: config.imgID,
-        key: config.imgKey,
-        q: keywords,
-        searchType: 'image',
-      },
-    };
+    saveData(
+      msg.id,
+      links,
+      msg.author.id,
+      command.options.reactionButtonTimeout,
+    );
 
-    const response = await got('https://www.googleapis.com/customsearch/v1', options);
-    const imgUrl = response.body.items[(Math.floor(Math.random() * (0 - 9 + 1)) + 9)].link;
-
-    return msg.channel.createMessage(`${author} searched for '${keywords}'~ ${imgUrl}`);
+    return msg.channel.createMessage(links[0]);
   },
 
   options: {
@@ -30,6 +23,7 @@ const command = {
     cooldown: 3000,
     description: 'Search for an image using Google Images!',
     usage: 'img anime girl',
+    reactionButtonTimeout: 120000,
   },
 };
-module.exports = command;
+module.exports = addReactionButtons(command, command.options.reactionButtonTimeout);
---
+++ bot/util/generate_image_links.js
@@ -0,0 +1,21 @@
+const { google } = require('googleapis');
+const config = require('../config/config.json');
+
+const search = google.customsearch('v1');
+
+const generateLinks = async (query) => {
+  const options = {
+    cx: config.imgID,
+    key: config.imgKey,
+    q: query,
+    searchType: 'image',
+  };
+
+  const response = await search.cse.list(options);
+
+  const links = response.data.items.map(item => item.link);
+
+  return links;
+};
+
+module.exports = generateLinks;
